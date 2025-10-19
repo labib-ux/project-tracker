75f5ca0065ff54d379a874ecea6975598444b312
labib-ux <128136230+labib-ux@users.noreply.github.com>
2025-10-20 02:14:33 +0600

    Initialize main.jsx with ReactDOM rendering
    Added src/main.jsx file

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RPPTDesignSystem from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RPPTDesignSystem />
+  </React.StrictMode>,
+);
